82ff5d769619d4f666adebf279d7360b998db184
Ayyker <niko23005@gmail.com>
2024-03-06 18:18:02 +0100

    test

--- TWP2024/Program.cs
+++ TWP2024/Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace TPW2024 {
     public class Pies {
 
-        Boolean isPies = true; //Czy wybrano psa?
+        Boolean isPies = true; //Czy wybrano psaa?
 
         public Boolean CheckPies () {
                 return isPies;
