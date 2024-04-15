24dc816a4988bcad3c707159cd4cd21810f7a5c8
arkadiuso000 <arkadiusz.mlyn@icloud.com>
2024-04-16 00:09:59 +0200

    ui logic

--- logic_layer/SimulationManager.cs
+++ logic_layer/SimulationManager.cs
@@ -10,6 +10,7 @@ namespace logic_layer
     public class SimulationManager
     {
         private IBallRepository ballRepository;
+        private List<ISimulationObserver> observers = new List<ISimulationObserver>();
         public SimulationManager(IBallRepository ballRepository)
         {
             this.ballRepository = ballRepository;
@@ -18,21 +19,45 @@ namespace logic_layer
         public void InitializeSimulation(int numberOfBalls, int width, int height)
         {
             ballRepository.InitializeBalls(numberOfBalls, width, height);
+            NotifyObservers();
         }
 
         public void UpdateSimulation()
         {
             ballRepository.UpdateBalls();
+            NotifyObservers();
         }
 
         public void AddBall(Ball ball)
         {
             ballRepository.AddBall(ball);
+            NotifyObservers();
         }
 
         public void RemoveBall(Ball ball)
         {
             ballRepository.RemoveBall(ball);
+            NotifyObservers();
+        }
+
+        public void RegisterObserver(ISimulationObserver observer)
+        {
+            if (!observers.Contains(observer))
+            {
+                observers.Add(observer);
+            }
+        }
+        public void RemoveObserver(ISimulationObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        protected void NotifyObservers()
+        {
+            foreach (ISimulationObserver observer in observers)
+            {
+                observer.Update();
+            }
         }
     }
 }
---
+++ presentation_layer/ViewModels/SimulationUI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using data_layer;
+using logic_layer;
+
+namespace presentation_layer.ViewModels
+{
+    public class SimulationUI : ISimulationObserver
+    {
+        public SimulationUI(SimulationManager manager)
+        {
+            this.simulationManager = manager;
+            simulationManager.RegisterObserver(this);
+        }
+
+        public void Update()
+        {
+            // Odśwież GUI w odpowiedzi na zmiany w symulacji
+            RefreshDisplay();
+        }
+
+        private void RefreshDisplay()
+        {
+            // Logika odświeżania interfejsu użytkownika
+        }
+
+        // Zapewnij możliwość odrejestrowania obserwatora, gdy GUI jest zamykane lub nie jest już potrzebne
+        public void Detach()
+        {
+            simulationManager.RemoveObserver(this);
+        }
+    }
+}
