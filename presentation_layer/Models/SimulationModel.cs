0003eaf1e1427d20b3e996cff7e09f904a0e2866
arkadiuso000 <arkadiusz.mlyn@icloud.com>
2024-05-05 11:51:59 +0200

    simulationModel added

---
+++ presentation_layer/Models/SimulationModel.cs
@@ -0,0 +1,40 @@
+﻿using data_layer;
+using logic_layer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace presentation_layer.Models
+{
+    public class SimulationModel
+    {
+        private IBallRepository _ballRepository;
+        private IBallManager _ballManager;
+
+        public SimulationModel(int width, int height)
+        {
+            _ballRepository = new BallRepository();
+            _ballManager = new BallManager(_ballRepository, width, height); 
+        }
+
+        public void GenerateBalls(int amount)
+        {
+            _ballManager.GenerateBalls(amount);
+        }
+
+        public void UpdateBalls() 
+        {
+            _ballManager.UpdateBalls();
+        }
+        public IReadOnlyList<Ball> GetBalls()
+        {
+            return _ballManager.GetBalls();
+        }
+        public void ClearAllBalls()
+        {
+            _ballManager.ClearAllBalls();
+        }
+    }
+}
