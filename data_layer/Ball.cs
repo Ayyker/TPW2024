fcdcdd3d1f060bcd544f18e360221b8450099535
arkadiuso000 <arkadiusz.mlyn@icloud.com>
2024-05-05 10:28:31 +0200

    ball rebuild

--- data_layer/Ball.cs
+++ data_layer/Ball.cs
@@ -6,45 +6,65 @@ using System.Threading.Tasks;
 
 namespace data_layer
 {
-    public class Ball: i_Ball
+    public class Ball: IBall
     {
         private int _ID;
         private double _X_position;
         private double _Y_position;
-        private double _
+        private double _Velocity_X;
+        private double _Velocity_Y;
+        private double _Radius;
 
-       
-        public double X { get; set; }
-        public double Y { get; set; }
-
-        
-        public double VelocityX { get; set; }
-        public double VelocityY { get; set; }
-
-        
-        public double Radius { get; set; }
-
-        
-        public Ball(double x, double y, double velocityX, double velocityY, double radius)
+        public Ball(int id, double x, double y, double velocityX, double velocityY, double radius)
         {
-            X = x;
-            Y = y;
-            VelocityX = velocityX;
-            VelocityY = velocityY;
+            ID = id;
+            X_position = x;
+            Y_position = y;
+            Velocity_X = velocityX;
+            Velocity_Y = velocityY;
             Radius = radius;
         }
 
-        
-        public void UpdatePosition()
+        public int ID
         {
-            X += VelocityX;
-            Y += VelocityY;
+            get => _ID; 
+            set => _ID = value;
         }
 
-        
+        public double X_position
+        {
+            get => _X_position;
+            set => _X_position = value;
+        }
+        public double Y_position
+        {
+            get => _Y_position;
+            set => _Y_position = value;
+        }
+
+        public double Velocity_X
+        {
+            get => _Velocity_X;
+            set => _Velocity_X = value;
+        }
+        public double Velocity_Y
+        {
+            get => _Velocity_Y;
+            set => _Velocity_Y = value;
+        }
+        public double Radius
+        {
+            get => _Radius;
+            set => _Radius = value;
+        }
+
+
+
+
+
         public override string ToString()
         {
-            return $"Ball at ({X}, {Y}) with velocity ({VelocityX}, {VelocityY}) and radius {Radius}.";
+            return $"Ball at ({X_position}, {Y_position}) with velocity ({Velocity_X}, {Velocity_Y}) and radius {Radius}.";
         }
     }
 }
