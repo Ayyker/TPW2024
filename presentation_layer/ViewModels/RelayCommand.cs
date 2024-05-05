06b7c9e91b82fcf2fa05f7b01148a86690872889
arkadiuso000 <arkadiusz.mlyn@icloud.com>
2024-05-05 11:55:46 +0200

    relay command changes

--- presentation_layer/ViewModels/RelayCommand.cs
+++ presentation_layer/ViewModels/RelayCommand.cs
@@ -1,41 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace presentation_layer.ViewModels
 {
     public class RelayCommand : ICommand
     {
-        readonly Action<object> _execute;
-        readonly Predicate<object> _canExecute;
+        private Action _execute;
+        private Func<bool> _canExecute;
 
-        public RelayCommand(object v)
+        public event EventHandler CanExecuteChanged
         {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
         }
 
-        public RelayCommand(Action<object> execute) : this(execute, null) 
-        { 
-        }
-
-        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
         {
-            _execute = execute ?? throw new ArgumentNullException("execute");
+            _execute = execute;
             _canExecute = canExecute;
         }
 
-        public event EventHandler CanExecuteChanged; //chlop w filmie cos tu ma
-
         public bool CanExecute(object parameter)
         {
-            return _canExecute == null || _canExecute(parameter);
+            return _canExecute == null || _canExecute();
         }
 
         public void Execute(object parameter)
         {
-            _execute(parameter);
+            _execute();
         }
     }
 }
