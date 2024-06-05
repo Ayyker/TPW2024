8d0376e3f3d66b51087ac584e88dc9d7b2ce0686
Ayyker <niko23005@gmail.com>
2024-06-05 18:13:59 +0200

    Update Logger.cs

--- data_layer/Logger.cs
+++ data_layer/Logger.cs
@@ -1,28 +1,24 @@
 ﻿using System.Text.Json;
 using System.Timers;
-using Timer = System.Timers.Timer;
 
 namespace data_layer {
     public class Logger {
-        private Timer timer;
-        private List<Ball> balls;
+        private System.Timers.Timer timer;
+        private readonly List<Ball> balls;
 
         public Logger(List<Ball> b) {
             this.balls = b;
             SetTimer();
-
         }
         private void OnTimedEvent(Object obj, ElapsedEventArgs e) {
-            using (StreamWriter streamWriter =
-                new StreamWriter(Directory.GetCurrentDirectory() + "\\ballLogs.txt", true)) {
-                string msg = ("Log from " + e.SignalTime + "\n");
-                foreach (Ball ball in balls) {
-                    streamWriter.WriteLine(msg + JsonSerializer.Serialize(ball));
-                }
+            using StreamWriter streamWriter = new StreamWriter(Directory.GetCurrentDirectory() + "\\Logs.txt", true);
+            string msg = ("Date: " + e.SignalTime + "\n");
+            foreach (Ball ball in balls) {
+                streamWriter.WriteLine(msg + JsonSerializer.Serialize(ball));
             }
         } 
         private void SetTimer() {
-            timer = new Timer(100);
+            timer = new System.Timers.Timer(100);
             timer.Elapsed += OnTimedEvent;
             timer.AutoReset = true;
             timer.Enabled = true;
